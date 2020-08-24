62e833392b8fdff3ea89e799f932c05828657d70
Andrii Nyzhnyk <andrij1510@gmail.com>
2020-08-24 23:03:09 +0300

    rename functions

--- src/main.rs
+++ src/main.rs
@@ -105,6 +105,7 @@ fn binary_search_vec(v: &Vec<i32>, find: i32) -> i32 {
     return -1;
 }
 
+
 fn run_selection_sort() {
     let v: Vec<i32> = vec![0, -10, 10, -4, -5];
     let sorted_vector: Vec<i32> = selection_sort(&v);
@@ -113,31 +114,31 @@ fn run_selection_sort() {
 
 
 fn selection_sort(v: &Vec<i32>) -> Vec<i32>{
-    let mut input_vector = v.clone();
+    let mut input_vector: Vec<i32> = v.clone();
     let mut sorted_vector: Vec<i32> = Vec::new();
 
     for _ in v.iter() {
-        let smallest = find_smallest(&input_vector);
-        println!("Input vector: {:#?}:", input_vector);
-        sorted_vector.push(input_vector[smallest]);
-        input_vector.swap_remove(smallest);
+        let smallest_element_with_index = find_smallest_element_into_vec(&input_vector);
+        sorted_vector.push(input_vector[smallest_element_with_index]);
+        input_vector.swap_remove(smallest_element_with_index);
     }
 
     return sorted_vector;
 }
 
-fn find_smallest(v: &Vec<i32>) -> usize {
+
+fn find_smallest_element_into_vec(v: &Vec<i32>) -> usize {
     let mut smallest: &i32 = &v[0];
-    let mut smallest_index: usize = 0;
+    let mut smallest_element_with_index: usize = 0;
 
     for (pos, elem) in v.iter().enumerate() {
         if elem < smallest {
             smallest = elem;
-            smallest_index = pos;
+            smallest_element_with_index = pos;
         }
     }
 
-    return smallest_index;
+    return smallest_element_with_index;
 }
 
 
